ae04cc412ff763c4d160cb8be60b9569ea6c638f
lucia <lucia12onica@gmail.com>
2023-04-03 19:31:10 -0500

    Added Student Assignment

---
+++ StudentProject/src/main/java/org/exampleConstructorInjection/Address.java
@@ -0,0 +1,25 @@
+package org.exampleConstructorInjection;
+
+public class Address {
+    private String city;
+    private String state;
+    private String country;
+    private String zipCode;
+
+    public Address(String city, String state, String country, String zipCode) {
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
+}
---
+++ StudentProject/src/main/java/org/exampleSetterInjection/Phone.java
@@ -0,0 +1,20 @@
+package org.exampleSetterInjection;
+
+public class Phone {
+    private String mob;
+
+    public String getMob() {
+        return mob;
+    }
+
+    public void setMob(String mob) {
+        this.mob = mob;
+    }
+
+    @Override
+    public String toString() {
+        return "Phone{" +
+                "mob='" + mob + '\'' +
+                '}';
+    }
+}
